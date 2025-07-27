1d0beebaebab4a322e4e524c7decf13fd4a83971
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-27 18:38:42 +0600

    type of,to fixed added

--- number.js
+++ number.js
@@ -1,8 +1,24 @@
 var orangePrice = 20;
 var choclate = 0.5;
-var applePrice = '30';
+var applePrice = parseInt('50');
+var applePrice = parseFloat('50.50');
 
 
 console.log(orangePrice);
 console.log(typeof orangePrice);
 console.log(orangePrice + applePrice);
+
+
+var first = 0.1;
+var second = 0.2;
+var total = (first + second);
+console.log(total);
+
+var firstt = 0.1;
+var secondd = 0.2;
+var totall = (firstt + secondd);
+console.log(totall.toFixed(2));
+
+
+const num = 50 / 0 ;
+console.log(num);
