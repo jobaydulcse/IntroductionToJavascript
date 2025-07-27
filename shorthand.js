76afb2337fe2809eff1bbcc9c775d1aad1cb0ab0
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-27 19:00:07 +0600

    shorthand added

---
+++ shorthand.js
@@ -0,0 +1,18 @@
+var price = 40 ;
+// price = price + 60 ;
+
+price += 10 ;
+console.log(price);
+
+
+var fridge = 40000 ;
+fridge -= 999 ;
+console.log(fridge);
+
+
+// maltiply
+var orangePrice = 40 ;
+// orangePrice = orangePrice * 5;
+orangePrice *=6
+console.log(orangePrice);
+
