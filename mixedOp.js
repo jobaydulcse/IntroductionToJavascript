385d831aea3e445ee6c096fa0b39c3432534c30d
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-27 19:09:08 +0600

    mixed op sdded

---
+++ mixedOp.js
@@ -0,0 +1,10 @@
+var first = 'Mobarok' ;
+var second = 'Tobarok' ;
+
+var fullName = first + second ;
+// console.log(fullName);
+// console.log(40+'3');
+
+
+console.log(40-'20');
+console.log('100'-10);
