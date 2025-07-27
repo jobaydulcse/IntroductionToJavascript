19296f664a428a97d4f8dc46e1c8c908677ffaec
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-27 13:04:03 +0600

    case sensative added

---
+++ naming.js
@@ -0,0 +1,25 @@
+// 1. no keyword name in variable name
+var false=45;
+var var =59;
+// 2.no space or gam in variable
+var is happpy= false;
+// 3.no quote
+var "address"="kochu khet";
+
+// 4.can not staart with a number but number  other than the first letter is allowed
+var 2money=347;
+// 5. variable is case sensative
+address 
+Address 
+ADDRESS 
+
+// 6.how to write a long variable name 
+
+var my current home address ="kalapara";
+var mycurrenthomeaddress ="kalapara";
+var my_current_home_address =  "kalapara";     
+// snake case
+var myCurrentHomeAddress ="kalapara";
+// camel case
+var MyCurrentHomeAddress ="kalapara";
+// pascal case
