c842f74cd215a9dbe89b93b0e1323b40c35d9a77
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-27 12:46:06 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,21 @@
+var name = "jobaydul";
+
+console.log(name);
+// string
+var name = "jobaydul";
+var address = " kalapra patuakhali";
+var country = " bangladesh";
+
+// boolean
+var isRice = true;
+console.log(isRice);
+
+var result = false;
+console.log(result);
+console.log(typeof result);
+console.log(typeof country);
+
+// number
+var price=4234;
+console.log(price);
+console.log(typeof price);
---
+++ variable.js
@@ -0,0 +1,10 @@
+var money =(37);
+var price =(392);
+
+
+console.log(money)
+console.log(price)
+
+var age = 25;
+age = 18;
+console.log(age)
